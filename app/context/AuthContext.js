a00c2e4cc407aec26bc48187244794a12c8faf07
JackBlair87 <jackblairing@gmail.com>
2024-07-22 12:16:22 -0700

    Update AuthContext.js

--- app/context/AuthContext.js
+++ app/context/AuthContext.js
@@ -3,6 +3,7 @@
 import { useContext, createContext, useState, useEffect } from "react";
 import {
   signInWithPopup,
+  signInWithRedirect,
   signOut,
   onAuthStateChanged,
   GoogleAuthProvider,
@@ -16,7 +17,7 @@ export const AuthContextProvider = ({ children }) => {
 
   const googleSignIn = () => {
     const provider = new GoogleAuthProvider();
-    signInWithPopup(auth, provider).then((result) => {
+    signInWithRedirect(auth, provider).then((result) => {
       if(window !== undefined) {
         console.log(result._tokenResponse.oauthIdToken);
         window.location.href = "second://" + result._tokenResponse.oauthIdToken;
